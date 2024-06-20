95a75bbbce0828ef71e61b85de9ee56078b1c375
aryanmishra64 <127666009+aryanmishra64@users.noreply.github.com>
2024-06-20 12:46:14 +0530

    Time: 152 ms (24.23%), Space: 63.1 MB (37.91%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+     
+        
+        int n = graph.size();
+        vector<int> vis(n,0);
+        vector<int> indegree(n,0);
+        vector<int> adj[n];
+       
+        for(int i=0; i<n; i++){
+            for(auto &it: graph[i]){
+                adj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            ans.push_back(node);
+            q.pop();
+           
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+      
+        sort(ans.begin(), ans.end());
+        return ans; 
+    }
+};
