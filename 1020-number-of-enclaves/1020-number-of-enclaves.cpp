fcc49a8eea5502bdecc766f039b390e1650b7723
aryanmishra64 <127666009+aryanmishra64@users.noreply.github.com>
2025-01-08 13:14:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,54 +1,67 @@
 class Solution {
 public:
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[row][col]=1;
+
+        int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
+
+        for(int i=0;i<4;i++){
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+
+            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                dfs(nrow,ncol,vis,grid);
+            }
+
+        }
+    }
+
+
+
     int numEnclaves(vector<vector<int>>& grid) {
-        queue<pair<int,int>> q; 
-        int n = grid.size(); 
-        int m = grid[0].size(); 
-         vector<vector<int>> vis(n, vector<int>(m, 0));
-        // traverse boundary elements
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                // first row, first col, last row, last col 
-                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
-                    // if it is a land then store it in queue
-                    if(grid[i][j] == 1) {
-                        q.push({i, j}); 
-                        vis[i][j] = 1; 
-                    }
+        int n=grid.size();
+        int m=grid[0].size();
+     vector<vector<int>>vis(n,vector<int>(m,0));
+
+
+       //first and last row 
+       
+        for(int j=0;j<m;j++){
+            if(!vis[0][j] && grid[0][j]==1){
+                dfs(0,j,vis,grid);
+            }
+            if(!vis[n-1][j] && grid[n-1][j]==1){
+                dfs(n-1,j,vis,grid);
+            }
+
+
+
+        }
+        // first col and last col
+        for(int i=0;i<n;i++){
+            if(!vis[i][0] && grid[i][0]==1){
+                dfs(i,0,vis,grid);
+            }
+            if(!vis[i][m-1] && grid[i][m-1]==1){
+                dfs(i,n-1,vis,grid);
+            }
+        }
+
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]!=1 && grid[i][j]==1){
+                    cnt++;
                 }
             }
         }
-        
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, +1, +0, -1}; 
-        
-        while(!q.empty()) {
-            int row = q.front().first; 
-            int col = q.front().second; 
-            q.pop(); 
-            
-            // traverses all 4 directions
-            for(int i = 0;i<4;i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i]; 
-                // check for valid coordinates and for land cell
-                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
-                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = 1; 
-                }
-            }
-            
-        }
-        
-        int cnt = 0;
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                // check for unvisited land cell
-                if(grid[i][j] == 1 & vis[i][j] == 0) 
-                    cnt++; 
-            }
-        }
-        return cnt; 
+        return cnt;
+
+
+       
     }
 };
