f8d6eebbbbe36bb8fdf59313b1fe392208bac6dc
aryanmishra64 <127666009+aryanmishra64@users.noreply.github.com>
2025-01-16 12:28:33 +0530

    Time: 17 ms (64.76%), Space: 26.2 MB (47.31%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
         int n=grid.size();
+
         if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
-        
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-        
-            queue<pair<int, pair<int, int>>> q;
-            q.push({0,{0,0}});
-        dist[0][0]=0;
-        
-        while(!q.empty())
-        {
-            auto it=q.front();
+
+
+        vector<vector<int>>vis(n,vector<int>(n,1e9));
+
+        vis[0][0]=0;
+
+        queue<pair<int, pair<int, int>>> q;
+
+        q.push({0,{0,0}});
+
+        while(!q.empty()){
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
             q.pop();
-            int d=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-            
-            for(int delrow=-1;delrow<=1;delrow++)
-            {
-                for(int delcol=-1;delcol<=1;delcol++)
-                {
-                    int nrow=row+delrow;
-                    int ncol=col+delcol;
-                     if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0 &&                               d+1<dist[nrow][ncol])
-                     {
-                         q.push({d+1,{nrow,ncol}});
-                         dist[nrow][ncol]=d+1;
-                     }
-              
+
+            for(int drow=-1;drow<=1;drow++){
+                for(int dcol=-1;dcol<=1;dcol++){
+                    int nrow=row+drow;
+                    int ncol=col+dcol;
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0 && dis+1<vis[nrow][ncol]){
+
+                        vis[nrow][ncol]=dis+1;
+                        q.push({dis+1,{nrow,ncol}});
+                    }
+
                 }
             }
+
         }
-        if(dist[n-1][n-1]==INT_MAX) return -1;
-        return dist[n-1][n-1]+1;
-        
-        
+        if(vis[n-1][n-1]==1e9) return -1;
+
+        return vis[n-1][n-1]+1;
+
+
         
     }
 };
