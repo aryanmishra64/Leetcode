288f38048f66738f1cb02e69155d89cfc0111a06
aryanmishra64 <127666009+aryanmishra64@users.noreply.github.com>
2025-09-28 21:50:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+    int n;
+    int solve(int i,int j,vector<vector<int>>& arr,vector<vector<int>>&dp){
+        if(i==n-1){
+            return arr[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int down=arr[i][j]+solve(i+1,j,arr,dp);
+        int dig=arr[i][j]+solve(i+1,j+1,arr,dp);
+
+        return dp[i][j]=min(down,dig);
+
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<int>front(n,0),cur(n,0);
         
-        for(int j=0;j<n;j++){
-            front[j]=triangle[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=i;j>=0;j--)
-            {
-                int d=triangle[i][j]+front[j];
-                int dg=triangle[i][j]+front[j+1];
-                
-                cur[j]=min(d,dg);
-                
-                
-            }
-           front=cur;
-        }
-        return front[0];
+        n=triangle.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
+        
+     int ans=   solve(0,0,triangle,dp);
+
+     return ans;
+
     }
 };
